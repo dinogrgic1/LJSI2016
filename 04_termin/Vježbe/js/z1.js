37de5434c1835c087cb5bc003eb5d914553b2431
Vlatko Vlahek <vlatko@prototyp.digital>
2016-08-25 15:16:57 +0200

    Scope and closures z1,z2

---
+++ 04_termin/Vježbe/js/z1.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// Dovrši funkciju
+// Zadatak: Funkcija randomise() treba vratiti nasumični cijeli broj, na način
+// 1. da izračuna broj znakova u nazivu "Osijek Software City"
+// 2. Pribroji broj znakova iz "Osijek Software City" nasumičnom cijelom broju
+// 3. Podijeli s 2 i zaokruži na cjeli broj ako smo dobili decimalnu vrijednost
+
+var x = "Osijek Software City";
+function randomise(vrijednost) {
+    var slucajniBroj; 
+    
+    // Napiši 1. funkciju koja vraća slučajni cijeli broj
+
+    // Napiši 2. funkciju koja dijeli trenutni rezultat na 2 i zaokružuje na cijeli broj ako je potrebno
+
+    // vrati -> slucajniBroj = duljina od "Osijek Software City) + random cijeli broj / 2.
+}
+
+// Ovdje pozovi funkciju randomise
